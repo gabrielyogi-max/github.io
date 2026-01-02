942479655442a632cd9c9a9238250c5788db57f4
gabrielyogi-max <ireneyee2205@gmail.com>
2026-01-01 23:01:11 -0300

    Update script.js

--- script.js
+++ script.js
@@ -152,7 +152,7 @@ class ManifoldBackground {
 
         this.mesh = new THREE.Mesh(geometry, material);
         this.mesh.rotation.x = -Math.PI * 0.35;
-        this.mesh.position.y = -15; // Higher up
+        this.mesh.position.y = -5; // Higher up
         this.mesh.position.z = -5;
         this.scene.add(this.mesh);
     }
@@ -251,3 +251,4 @@ document.addEventListener('DOMContentLoaded', () => {
     const bg = new ManifoldBackground();
     new ThemeManager(bg);
 });
+
